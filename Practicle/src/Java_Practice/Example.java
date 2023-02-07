e0bc09a329db5382e10322092fc1cefa35087097
Vishal <gorevishal758@gmail.com>
2023-02-07 18:03:25 +0530

    practice

---
+++ Practicle/src/Java_Practice/Example.java
@@ -0,0 +1,28 @@
+package Java_Practice;
+
+class PQR
+{
+	public void method()
+	{
+		System.out.println("KING");
+	}
+}
+
+public class Example {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		PQR C=new PQR();
+		
+		C.method();
+		
+	int i=3;
+	
+	if(i>=2)
+	{
+		System.out.println("VISHAL GORE");
+	}
+	
+	}
+
+}
